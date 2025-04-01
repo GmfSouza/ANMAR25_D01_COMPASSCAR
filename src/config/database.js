5a42757dbbcc2954f34841d651addc4790c40fc2
Gabryel <gabryel.macedo@gmail.com>
2025-04-01 19:07:25 -0300

    feat(config):configuring the database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+	host: "localhost",
+	dialect: "mysql",
+	username: "root",
+	password: "1*4G$E4k*7J*",
+	database: "compasscar",
+	define: {
+		timestamp: true,
+		underscored: true,
+	},
+};
