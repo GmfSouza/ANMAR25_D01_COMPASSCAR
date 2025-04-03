619a9dd8e90d8d6551633fc5eaa8289bea2cf4a1
Gabryel <gabryel.macedo@gmail.com>
2025-04-03 19:48:37 -0300

    feat(api): implementing GET /api/v1/cars/:id to search for cars by id

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 		let carItems = [];
 		if (items && items.length > 0) {
 			carItems = await Promise.all(
-				items.map((itemName) => 
+				items.map((itemName) =>
 					CarItem.create({
 						name: itemName,
 						car_id: car.id,
@@ -30,32 +30,55 @@ router.post("/", async (req, res) => {
 			...car.toJSON(),
 			carItems: carItems.map((item) => item.toJSON()),
 		});
-
 	} catch (error) {
-
 		console.error("Error creating car:", error);
-		res.status(400).json({ error: "Failed to create car"});
-	  }
+		res.status(400).json({ error: "Failed to create car" });
+	}
 });
 
-
 router.get("/", async (req, res) => {
-	try{
+	try {
 		const cars = await Car.findAll({
-			include: [{
-				model: CarItem,
-				as: "items",
-			}]
-		})
-
-		res.status(200).json(cars)
+			include: [
+				{
+					model: CarItem,
+					as: "items",
+				},
+			],
+		});
+		res.status(200).json(cars);
 	} catch (error) {
-		console.error("Error when searching for cars", error)
-		res.status(404).json({error: "car not found", details: error.message})
+		console.error("Error when searching for cars:", error);
+		res
+			.status(500)
+			.json({ error: "Failed to search cars", details: error.message });
 	}
-	
+});
+
+router.get("/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		const car = await Car.findByPk(id, {
+			include: [
+				{
+					model: CarItem,
+					as: "items",
+				},
+			],
+		});
+
+		if (!car) {
+			return res.status(404).json({ error: "Car not found." });
+		}
+
+		res.status(200).json(car);
+	} catch (error) {
+		console.error("Error when searching for car", error);
+		res
+			.status(500)
+			.json({ error: "Failed to search car", details: error.message });
+	}
 });
 
 module.exports = router;
-
-
