29bab9ebfc2dda539a55986501c77de56247b6f6
Gabryel <gabryel.macedo@gmail.com>
2025-04-03 16:45:03 -0300

    feat(api): add POST /api/v1/car endpoint

--- src/routes/carRoutes.js
+++ src/routes/carRoutes.js
@@ -1,8 +1,48 @@
-const express = require('express')
-const router  = express.Router()
+const express = require("express");
+const router = express.Router();
+const Car = require("../models/car.js");
+const CarItem = require("../models/carItem.js");
 
-router.get('/', (req, res) => {
-    res.send('is working')
-})
+router.post("/", async (req, res) => {
+	try {
+		const { brand, model, plate, year, items } = req.body;
 
-module.exports = router
+		const car = await Car.create({
+			brand,
+			model,
+			plate,
+			year,
+		});
+
+		let carItems = [];
+		if (items && items.length > 0) {
+			carItems = await Promise.all(
+				items.map((itemName) => 
+					CarItem.create({
+						name: itemName,
+						car_id: car.id,
+					})
+				)
+			);
+		}
+
+		res.status(201).json({
+			...car.toJSON(),
+			carItems: carItems.map((item) => item.toJSON()),
+		});
+
+	} catch (error) {
+
+		console.error("Error creating car:", error);
+		res.status(400).json({ error: "Failed to create car"});
+	  }
+});
+
+
+router.get("/", (req, res) => {
+	res.send("GET /api/v1/cars is running!");
+});
+
+module.exports = router;
+
+
